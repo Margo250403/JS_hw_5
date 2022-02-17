3ec5a4aef06f3bd5b5cc2909f285531c46e35f7b
Margo250403 <margoshakac@gmail.com>
2022-02-17 23:29:02 +0200

    add finished changes

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 function wrapTags(str, tag) 
 {
-     var result = "";
-     return result += `<${tag}> ${str} </${tag}>`;
+     return `<${tag}> ${str} </${tag}>`;
 }
 var res = wrapTags("Do not give up, the beginning is always the hardest", "mark");
 
